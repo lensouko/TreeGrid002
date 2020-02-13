9d9551752b3f9f14664271a36e8fe3efbe1df24a
煉 <len@nya-nya.net>
2020-02-14 01:59:51 +0900

    jsonからデータを作成するように変更 参考元から構造を変更したために不要となったプロパティやコンストラクタを削除

--- TreeGrid002/MainViewModel.cs
+++ TreeGrid002/MainViewModel.cs
@@ -1,33 +1,51 @@
 ﻿using Prism.Mvvm;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
 
 namespace TreeGrid002
 {
     public class MainViewModel : BindableBase
     {
-        public List<Person> People { get; set; } = new List<Person>
-        {
-            new Person{Name="天財太郎", Age=19},
-            new Person{Name="天財次郎", Age=17,},
-            new Person{Name="天財女王", Age=54, Childs=new List<Person>
-            {
-                new Person{Name="天財息子", Age=29},
-                new Person{Name="天財娘", Age=27,Childs=new List<Person>
-                {
-                    new Person { Name= "天財孫娘", Age=3 },
-                    new Person { Name= "天財孫", Age=1 },
-                } },
-                new Person{Name="天財末子", Age=16},
-            } },
-        };
+        private List<Person> people = new List<Person>();
+        //{
+        //    new Person{Name="天財太郎", Age=19},
+        //    new Person{Name="天財次郎", Age=17,},
+        //    new Person{Name="天財女王", Age=54, Childs=new List<Person>
+        //    {
+        //        new Person{Name="天財息子", Age=29},
+        //        new Person{Name="天財娘", Age=27,Childs=new List<Person>
+        //        {
+        //            new Person { Name= "天財孫娘", Age=3 },
+        //            new Person { Name= "天財孫", Age=1 },
+        //        } },
+        //        new Person{Name="天財末子", Age=16},
+        //    } },
+        //};
+        //テストデータを作成する場合は上記のコメントを解除する
 
+        /// <summary>
+        /// 表示用に入れ子データを組みなおして1次元リストにしたもの
+        /// </summary>
         public ObservableCollection<PersonItem> PeopleItems { get; private set; } = new ObservableCollection<PersonItem>();
 
 
         public MainViewModel()
         {
-            addChildItem(People);
+            //テストデータを作成する場合は以下のコメントを解除する
+            //var JSoptions = new JsonSerializerOptions
+            //{
+            //    Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
+            //    WriteIndented = true
+            //};
+            //var json = JsonSerializer.Serialize(People, JSoptions);
+            //File.WriteAllText("data.json", json);
+            people.Clear();
+            people.AddRange(JsonSerializer.Deserialize<List<Person>>(File.ReadAllText("data.json")));
+            addChildItem(people);
         }
 
         private void addChildItem(IReadOnlyList<Person> childs, PersonItem parent = null)
--- TreeGrid002/Person.cs
+++ TreeGrid002/Person.cs
@@ -1,69 +1,18 @@
-﻿using Prism.Mvvm;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Windows;
+﻿using System.Collections.Generic;
 
 namespace TreeGrid002
 {
-    // DataGridに表示するデータ
-    public class Person : BindableBase
+    /// <summary>
+    /// 入れ子データ
+    /// </summary>
+    public class Person
     {
-
         // 名前
-        private string name;
-        public string Name 
-        {
-            get => $"{new string('　',NestLevel*2)}{name}"; 
-            set => name = value; 
-        }
+        public string Name { get; set; }
 
         // 年齢
-        private int age;
-        public int Age
-        {
-            get => age;
-            set
-            {
-                if (value < 0)
-                {
-                    MessageBox.Show("範囲外の値が設定されました。");
-                    RaisePropertyChanged(nameof(Age));
-                }
-                else
-                {
-                    SetProperty(ref age, value);
-                }
-            }
-        }
-
-        private bool isOpen = true;
-        public bool IsOpen
-        {
-            get => (parent?.isOpen ?? true) && isOpen;
-            set => SetProperty(ref isOpen, value);
-        }
-
-        public Visibility Visibility => (parent?.IsOpen ?? true) ? Visibility.Visible : Visibility.Collapsed;
+        public int Age { get; set; }
 
         public List<Person> Childs { get; set; } = new List<Person>();
-
-        private Person parent = null;
-
-        public int NestLevel
-        {
-            get
-            {
-                return (parent?.NestLevel ?? -1) + 1;
-            }
-        }
-
-        public Person() { }
-
-        // 子階層と親階層を設定する
-        public Person(List<Person> child = null, Person parent = null)
-        {
-            Childs.AddRange(child);
-            this.parent = parent;
-        }
     }
 }
